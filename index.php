2438d20b998182f09920d81422e70b57f42f76c2
Danil Dream <33546123+danildream@users.noreply.github.com>
2018-03-22 14:52:17 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,430 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Главная - Севастополь</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- Link Swiper's CSS -->
+    <link rel="stylesheet" href="http://idangero.us/swiper/dist/css/swiper.min.css">
+
+
+    <!-- Custom styles for this template -->
+    <link href="style.css" rel="stylesheet">
+    <link href="media.css" rel="stylesheet">
+
+    <!-- Swiper JS -->
+    <script src="plugins/swiper/swiper.js"></script>
+
+
+  </head>
+
+  <body>
+    <header class="container">
+      <div class="row">
+        <div class="logo col-2 pl-0">
+          <img src="img/logo.png" alt="">
+        </div>
+        <div class="col-8 p-0">
+          <div class="top-menu row">
+            <div class="nav-item col-sm">
+              <a class="nav-link h5" href="#">Главная</a>
+            </div>
+            <div class="nav-item col-sm">
+              <a class="nav-link h5" href="#">О проекте</a>
+            </div>
+            <div class="nav-item col-sm">
+              <a class="nav-link h5" href="#">Материалы</a>
+            </div>
+            <div class="nav-item col-sm">
+              <a class="nav-link h5" href="#">Эксперты</a>
+            </div>
+            <div class="nav-item col-sm">
+              <a class="nav-link h5" href="#">Контакты</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-2 d-flex align-items-center p-0">
+          <div class="col-8 lang d-flex justify-content-end">
+            <button class="btn btn-secondary dropdown-toggle h5" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            RU
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+             <a class="dropdown-item" href="#">EN</a>
+             <a class="dropdown-item" href="#">DE</a>
+             <a class="dropdown-item" href="#">FR</a>
+            </div>
+          </div>
+          <div class="col-4 pr-0 search">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <img src="img/search.png" alt="">
+            </button>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+              <input type="search" class="form-control" placeholder="введите слово">
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </header>
+    <main role="main">
+      <div class="map">
+        <div class="container">
+          <div class="region row">
+            <div class="col-3 icon p-0">
+              <img src="img/menu-button.png" alt="">
+            </div>
+            <div class="col-9 p-0">
+              <a href="#">Выберите регион</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container map_block">
+        <div class="row">
+           <div class="col-5 map_one-block map-title">
+             Геостратегический атлас Большого Средиземноморья
+           </div>
+           <a href="#" class="map-more col-3 map_one-block "
+                style="background: url(../html/img/about.png);">
+             Подробнее <br> о проекте
+           <img class="map-arrow" src="img/arrow.png" alt="">
+           </a>
+           <a href="#" class="map-more col-3 map_one-block "
+                style="background: url(../html/img/atlas.png);"  >
+             Возможности <br> атласа
+           <img class="map-arrow" src="img/arrow.png" alt="">
+           </a>
+        </div>
+      </div>
+      <div class="description">
+        <div class="container ">
+          <div class="description_item col-md-10">
+            «Геостратегический атлас Большого Средиземноморья» - это уникальный междисциплинарный проект, сочетающий подготовку научно-аналитических текстов и статистических материалов с инфографическими и картографическими разработками по проблемам государств Большого Средиземноморья – стран, располагающихся в бассейнах Азовского, Черного и Средиземного морей.
+          </div>
+        </div>
+      </div>
+      <div class="slider last">
+        <div class="title container">
+          Название блока
+          <div class="line">__</div>
+        </div>
+        <div class="swiper-container">
+           <div class="swiper-wrapper">
+             <div class="swiper-slide">
+              <img src="img/cont_img.png" alt="">
+              <div class="caption">
+                 <div class="title-caption">
+                   1 Российское государство и Крым в Большом Средиземноморье
+                 </div>
+                 <div class="text-caption">
+                   XV - конец XVIII века
+                 </div>
+               </div>
+             </div>
+             <div class="swiper-slide">
+              <img src="img/cont_img.png" alt="">
+              <div class="caption">
+                 <div class="title-caption">
+                   2 Российское государство и Крым в Большом Средиземноморье
+                 </div>
+                 <div class="text-caption">
+                   XV - конец XVIII века
+                 </div>
+               </div>
+             </div>
+             <div class="swiper-slide">
+              <img src="img/cont_img.png" alt="">
+              <div class="caption">
+                 <div class="title-caption">
+                   3 Российское государство и Крым в Большом Средиземноморье
+                 </div>
+                 <div class="text-caption">
+                   XV - конец XVIII века
+                 </div>
+               </div>
+             </div>
+             <div class="swiper-slide">
+              <img src="img/cont_img.png" alt="">
+              <div class="caption">
+                 <div class="title-caption">
+                   4 Российское государство и Крым в Большом Средиземноморье
+                 </div>
+                 <div class="text-caption">
+                   XV - конец XVIII века
+                 </div>
+               </div>
+             </div>
+             <div class="swiper-slide">
+              <img src="img/cont_img.png" alt="">
+              <div class="caption">
+                 <div class="title-caption">
+                   5 Российское государство и Крым в Большом Средиземноморье
+                 </div>
+                 <div class="text-caption">
+                   XV - конец XVIII века
+                 </div>
+               </div>
+             </div>
+             <div class="swiper-slide">
+              <img src="img/cont_img.png" alt="">
+              <div class="caption">
+                 <div class="title-caption">
+                   6 Российское государство и Крым в Большом Средиземноморье
+                 </div>
+                 <div class="text-caption">
+                   XV - конец XVIII века
+                 </div>
+               </div>
+             </div>
+             <div class="swiper-slide">
+              <img src="img/cont_img.png" alt="">
+              <div class="caption">
+                 <div class="title-caption">
+                   7 Российское государство и Крым в Большом Средиземноморье
+                 </div>
+                 <div class="text-caption">
+                   XV - конец XVIII века
+                 </div>
+               </div>
+             </div>
+             <div class="swiper-slide">
+              <img src="img/cont_img.png" alt="">
+              <div class="caption">
+                 <div class="title-caption">
+                   8 Российское государство и Крым в Большом Средиземноморье
+                 </div>
+                 <div class="text-caption">
+                   XV - конец XVIII века
+                 </div>
+               </div>
+             </div>
+             <div class="swiper-slide">
+              <img src="img/cont_img.png" alt="">
+              <div class="caption">
+                 <div class="title-caption">
+                   9 Российское государство и Крым в Большом Средиземноморье
+                 </div>
+                 <div class="text-caption">
+                   XV - конец XVIII века
+                 </div>
+               </div>
+             </div>
+             <div class="swiper-slide">
+              <img src="img/cont_img.png" alt="">
+              <div class="caption">
+                 <div class="title-caption">
+                   10 Российское государство и Крым в Большом Средиземноморье
+                 </div>
+                 <div class="text-caption">
+                   XV - конец XVIII века
+                 </div>
+               </div>
+             </div>
+             <div class="swiper-slide">
+              <img src="img/cont_img.png" alt="">
+              <div class="caption">
+                 <div class="title-caption">
+                   11 Российское государство и Крым в Большом Средиземноморье
+                 </div>
+                 <div class="text-caption">
+                   XV - конец XVIII века
+                 </div>
+               </div>
+             </div>
+             <div class="swiper-slide">
+              <img src="img/cont_img.png" alt="">
+              <div class="caption">
+                 <div class="title-caption">
+                   12 Российское государство и Крым в Большом Средиземноморье
+                 </div>
+                 <div class="text-caption">
+                   XV - конец XVIII века
+                 </div>
+               </div>
+             </div>
+             <div class="swiper-slide">
+              <img src="img/cont_img.png" alt="">
+              <div class="caption">
+                 <div class="title-caption">
+                   13 Российское государство и Крым в Большом Средиземноморье
+                 </div>
+                 <div class="text-caption">
+                   XV - конец XVIII века
+                 </div>
+               </div>
+             </div>
+             <div class="swiper-slide">
+              <img src="img/cont_img.png" alt="">
+              <div class="caption">
+                 <div class="title-caption">
+                   14 Российское государство и Крым в Большом Средиземноморье
+                 </div>
+                 <div class="text-caption">
+                   XV - конец XVIII века
+                 </div>
+               </div>
+             </div>
+             <div class="swiper-slide">
+              <img src="img/cont_img.png" alt="">
+              <div class="caption">
+                 <div class="title-caption">
+                   15 Российское государство и Крым в Большом Средиземноморье
+                 </div>
+                 <div class="text-caption">
+                   XV - конец XVIII века
+                 </div>
+               </div>
+             </div>
+             <div class="swiper-slide">
+              <img src="img/cont_img.png" alt="">
+              <div class="caption">
+                 <div class="title-caption">
+                   16 Российское государство и Крым в Большом Средиземноморье
+                 </div>
+                 <div class="text-caption">
+                   XV - конец XVIII века
+                 </div>
+               </div>
+             </div>
+
+           </div>
+           <!-- Add Pagination -->
+           <div class="swiper-button-next"></div>
+           <div class="swiper-button-prev"></div>
+         </div>
+      </div>
+        <div class="articles last">
+          <div class="title  container">
+            Последние материалы
+            <div class="line">__</div>
+          </div>
+          <div class="container">
+            <div class="row">
+              <div class="col-9 pl-0 articles_list">
+                <div class="card-columns">
+                  <div class="card text-center item">
+                     <a href="#"><img src="img/last_1.png">
+                       <div class="wrapper">
+                          <p class="title">Идеи гедонизма занимают центральное место в утилитаризме</p>
+                          <p class="date">15 января</p>
+                       </div>
+                    </a>
+                   </div>
+                  <div class="card text-center item second">
+                     <a href="#"><img src="img/last_1.png">
+                       <div class="wrapper">
+                          <p class="title">Идеи гедонизма занимают центральное место в утилитаризме</p>
+                          <p class="date">15 января</p>
+                       </div>
+                    </a>
+                   </div>
+                  <div class="card text-center item second">
+                     <a href="#"><img src="img/last_1.png">
+                       <div class="wrapper">
+                          <p class="title">Идеи гедонизма занимают центральное место в утилитаризме</p>
+                          <p class="date">15 января</p>
+                       </div>
+                    </a>
+                   </div>
+                   <div class="card text-center item">
+                      <a href="#"><img src="img/last_1.png">
+                        <div class="wrapper">
+                           <p class="title">Идеи гедонизма занимают центральное место в утилитаризме</p>
+                           <p class="date">15 января</p>
+                        </div>
+                     </a>
+                    </div>
+                  <div class="card text-center item second">
+                     <a href="#"><img src="img/last_1.png">
+                       <div class="wrapper">
+                          <p class="title">Идеи гедонизма занимают центральное место в утилитаризме</p>
+                          <p class="date">15 января</p>
+                       </div>
+                    </a>
+                   </div>
+                   <div class="card text-center item third">
+                      <a href="#"><img src="img/last_1.png">
+                        <div class="wrapper">
+                           <p class="title">Идеи гедонизма занимают центральное место в утилитаризме</p>
+                           <p class="date">15 января</p>
+                        </div>
+                     </a>
+                    </div>
+                </div>
+              </div>
+              <div class="col-3 articles_list">
+                <div class="item">
+                  <p class="title">Идеи гедонизма занимают центральное место в утилитаризме</p>
+                  <p class="date">15 января</p>
+                </div>
+                <div class="item">
+                  <p class="title">Идеи гедонизма занимают центральное место в утилитаризме</p>
+                  <p class="date">15 января</p>
+                </div>
+                <div class="item">
+                  <p class="title">Идеи гедонизма занимают центральное место в утилитаризме</p>
+                  <p class="date">15 января</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </main><!-- /.container -->
+
+    <!-- Initialize Swiper -->
+    <script>
+      var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 4,
+      slidesPerColumn: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 30,
+      centeredSlides: true,
+      slidesOffsetBefore: -400,
+      width:2000,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+//         breakpoints: {
+//              1024: {
+//                slidesPerView: 4,
+//                spaceBetween: 40,
+//              },
+//              768: {
+//                slidesPerView: 3,
+//                spaceBetween: 30,
+// slidesPerColumn: 1,
+//
+//
+// centeredSlides: false,
+//              },
+//              640: {
+//                slidesPerView: 2,
+//                spaceBetween: 20,
+//              },
+//              320: {
+//                slidesPerView: 1,
+//                spaceBetween: 10,
+//
+//              }
+//            }
+      });
+    </script>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+  </body>
+</html>
